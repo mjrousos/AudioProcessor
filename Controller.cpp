68961a83cbc0f75f17ad12f13430536f47639f7f
Mike Rousos <mikerou@microsoft.com>
2015-12-26 21:28:44 -0800

    Swap asctime in for asctime_s (since the latter isn't standard C++)

--- Controller.cpp
+++ Controller.cpp
@@ -14,8 +14,7 @@ const int TIME_STR_LEN = 28;
 void Log(const char* msg)
 {
     time_t t = time(NULL);
-    char timeStr[TIME_STR_LEN];
-    asctime_s(timeStr, TIME_STR_LEN, localtime(&t));
+    char* timeStr = asctime(localtime(&t));
     timeStr[strlen(timeStr) - 1] = '\0'; // asctime automatically  includes an unwanted new line; so get rid of it
     printf("[%s] %s\n", timeStr, msg);
 }
